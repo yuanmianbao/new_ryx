f86d5ebf1081a4a9047a3879cdacc8ab18e056b5
Terry Yuan <TerryYuan@TerrydeMacBook-Air.local>
2022-11-16 19:27:00 -0500

    first commit

---
+++ R/ryx.R
@@ -0,0 +1,41 @@
+#'@title ryx
+#'
+#'@description
+#'This \code{ryx} function can create a ryx type.
+#'
+#'This is wonderful.
+
+#'@return ryx
+#'@export
+#'@examples
+#'\dontrun{
+#'library(MASS)
+#'myryx <- ryx(Boston, y="medv")
+#'}
+
+
+ryx <- function(data, y, x){
+  if(missing(x)){
+    x <- names(data)[sapply(data, class)=="numeric"]
+    x <- setdiff(x, y)
+  }
+  df <- data.frame()
+  for (var in x){
+    res <- cor.test(data[[y]], data[[var]])
+    df_temp <- data.frame(variable = var,
+                          r = res$estimate,
+                          p = res$p.value)
+    df <- rbind(df, df_temp)
+    df <- df[order(-abs(df$r)),]
+  }
+
+  df$sigif <- ifelse(df$p < .001, "***",
+                     ifelse(df$p < .01, "**",
+                            ifelse(df$p < .05, "*", " ")))
+  results <- list(y=y, x=x, df=df)
+  class(results) <- "ryx"
+  return(results)
+}
+
+
+
---
+++ R/ryx.package.R
@@ -0,0 +1,9 @@
+#'@title ryx package
+#'
+#'@description
+#'ryx provides tools for exploring the correlation among the variables
+#'in a data set
+#'@docType package
+#'@name ryx-package
+NULL
+
